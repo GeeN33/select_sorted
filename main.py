9a84864f15b0953b88cbddc7e5e73f3fa148a8a5
Evgeniy <eugtrader@gmail.com>
2023-01-06 12:31:58 +0300

    добавление Алгоритмы Кэширование

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ def parsing_csv(limit):
     """
     filename = 'all_stocks_5yr.csv'
     i = 0
-    all_stocks = []
+    cache =  []
     with open(filename, newline='', encoding='utf-8') as f:
         reader = csv.DictReader(f, delimiter=',', skipinitialspace=True)
         for row in reader:
-            all_stocks.append(row)
+            cache.append(row)
             i += 1
             if i > limit:
                 break
 
-    return  all_stocks
+    return  cache
 
 def save_csv(dicts, filename):
     """
@@ -60,38 +60,42 @@ def select_sorted(sort_columns,limit, order, filename):
     """
      функция где происходит сортировка
     """
-    new_list = []
-    all_stocks = parsing_csv(limit)
-    for i in range(1,len(all_stocks)):
+    new_cache = []
+    cache = parsing_csv(limit)
+    for i in range(1,len(cache)):
         index = 0
         if order == 'desc':
-             index = find_max(all_stocks,sort_columns)
+             index = find_max(cache,sort_columns)
         else:
-            index = find_min(all_stocks, sort_columns)
-        new_list.append(all_stocks.pop(index))
+            index = find_min(cache, sort_columns)
+        new_cache.append(cache.pop(index))
 
-    save_csv(new_list, filename)
-    return  new_list
+    save_csv(new_cache, filename)
+    return  new_cache
 
 def get_by_date(date, name, filename):
     """
     функция где происходит поиск
     """
-    new_list = []
-    all_stocks = parsing_csv(limit=2000000)
-    for stock in all_stocks:
+    new_cache = []
+    cache = parsing_csv(limit=2000000)
+    for stock in cache:
         if date == stock['date'] and name == stock['Name']:
-            new_list.append(stock)
+            new_cache.append(stock)
 
-    save_csv(new_list, filename)
-    return new_list
+    save_csv(new_cache, filename)
+    return new_cache
 
 
 if __name__ == '__main__':
 
-    sorted_list = select_sorted(sort_columns ='low',limit=10, order='desc', filename='dump_select_sorted.csv')
+    # sorted_list = select_sorted(sort_columns ='high',limit=10, order='asc', filename='dump_select_sorted1.csv')
+    # sorted_list = select_sorted(sort_columns ='close',limit=10, order='asc', filename='dump_select_sorted2.csv')
+    # sorted_list = select_sorted(sort_columns ='low',limit=10, order='desc', filename='dump_select_sorted3.csv')
 
-    # sorted_list = get_by_date(date='2017-08-08', name='PCLN', filename='dump_get_by_date.csv')
+    sorted_list = get_by_date(date='2017-08-08', name='PCLN', filename='dump_get_by_date.csv')
+
+
 
     for list in sorted_list:
         print(list)
